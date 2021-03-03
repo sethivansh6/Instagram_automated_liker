cbd70448647e7a97d347e9819814624bd9f2f284
Vansh Sethi <sethivansh6@gmail.com>
2021-03-04 00:21:03 +0530

    automated_insta_post_liker

---
+++ app.js
@@ -0,0 +1,48 @@
+require("dotenv").config()
+const puppeteer = require("puppeteer")
+console.log(process.env.email)
+
+async function call(noOfPost) {
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  await page.goto("https://www.instagram.com/", { waitUntil: "networkidle2" })
+  await page.waitForSelector(".HmktE", { visible: true })
+  await page.type("input[name='username']", process.env.email, {
+    delay: 100,
+  })
+  await page.type("input[name='password']", process.env.password, {
+    delay: 100,
+  })
+
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "networkidle2" }),
+    page.click("button[type='submit']"),
+  ])
+
+  await page.type("input[placeholder='Search']", "abdevilliers17")
+  await page.waitForSelector("._01UL2 .fuqBx a", { visible: true })
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "networkidle2" }),
+    page.click(".-qQT3"),
+  ])
+
+  await page.waitForSelector("._9AhH0", { visible: true })
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "networkidle2" }),
+    page.click("._9AhH0"),
+  ])
+  let i = 0
+  do {
+    await page.waitForSelector(".fr66n button")
+    await page.click(".fr66n button")
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: "networkidle2" }),
+      page.click("._65Bje.coreSpriteRightPaginationArrow"),
+    ])
+    i++
+  } while (i < noOfPost)
+
+  await browser.close()
+}
+
+call(10)
