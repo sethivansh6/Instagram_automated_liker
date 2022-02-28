3909c73a4dcefb2b038c7b370fc0f885415b2870
Vansh Sethi <sethivansh6@gmail.com>
2022-02-28 14:46:05 +0530

    added remainder and product function in new.js

--- new.js
+++ new.js
@@ -2,3 +2,11 @@ console.log("This new file");
 //Adding a new line!
 //Adding yet another new line!
 //Will keep adding line!
+
+function product(p1, p2) {
+  return p1 * p2;
+}
+
+function remainder(p1, p2) {
+    return p1 % p2;
+}
